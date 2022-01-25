b9aeacbf7726f1c032595121e4100e8910673c7f
Rpuri2007 <rpuri@eastsideprep.org>
2022-01-25 14:44:55 -0800

    virtualpet

---
+++ Virtual Pet/js/pet.js
@@ -0,0 +1,15 @@
+let rowFaster = 0;
+let rowTogether = 0;
+let rowOnCourse = 0;
+
+function fasterButton() {
+    console.log("Going faster...")
+}
+
+function togetherButton() {
+    console.log("Sticking together...")
+}
+
+function courseButton() {
+    console.log("Staying on course...")
+}
